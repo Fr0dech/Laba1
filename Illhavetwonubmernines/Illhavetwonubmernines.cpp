3fe2bb2f93de9f979f1e1df6bcc8bf1709fec71e
Fr0dech <Fr0dech@DESKTOP-CNTNKIQ>
2022-09-08 21:41:07 +0200

    Добавьте файлы проекта.

---
+++ Illhavetwonubmernines/Illhavetwonubmernines.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Enter angle, first side's length and second side's length:" << std::endl;
+    double a;
+    int A, B;
+    std::cin >> a >> A >> B;
+    std::cout << "The area of the triangle is " << 0.5 * A * B * sin(a * 3.14159265 / 180);
+}
