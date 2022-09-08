4497da478e23a7ea20600d1a6e98480b039fa1cc
Fr0dech <Fr0dech@DESKTOP-CNTNKIQ>
2022-09-08 21:41:07 +0200

    Fixed length using only integer values.

--- Illhavetwonubmernines/Illhavetwonubmernines.cpp
+++ Illhavetwonubmernines/Illhavetwonubmernines.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
+#define PI = 3.14159265
 
 int main()
 {
-    std::cout << "Enter angle, first side's length and second side's length:" << std::endl;
-    double a;
-    int A, B;
+    std::cout << "Enter angle, first side's length and second side's length: ";
+    float a, A, B;
     std::cin >> a >> A >> B;
-    std::cout << "The area of the triangle is " << 0.5 * A * B * sin(a * 3.14159265 / 180);
+    std::cout << std::endl << "The area of the triangle is " << 0.5 * A * B * sin(a * PI / 180);
 }
